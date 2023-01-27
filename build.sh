20f495bbb1bbd6af0031aeea4af7632cfa42cf60
tanmoy7989 <tanmoy.7989@gmail.com>
2023-01-26 17:23:31 -0800

    auto_commit_msg_commit_24946

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ bundle exec jekyll build --incremental
 sync with git
 git add *
 if [ $# -eq 0 ]; then
-    msg=auto_commit_msg_$(echo $RANDOM | md5sum | head -c 20)
+    msg=auto_commit_msg_$(echo commit_$RANDOM)
 else
     msg=$1
 fi
