45dab7281e3dff60d696bdae8fa0900837b2ec4b
tanmoy7989 <tanmoy.7989@gmail.com>
2023-01-31 00:56:47 -0800

    auto_commit_msg_5677

---
+++ _site/update.sh
@@ -0,0 +1,28 @@
+ENVNAME=webenv
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda activate $ENVNAME
+
+# setup the correct themes
+rm -f Gemfile.lock
+bundle
+
+# build resume
+curr_dir=$(pwd)
+mkdir -p assets/docs/resume
+cd resume && npm run export
+mv resume.pdf ../assets/docs/resume/
+cd $curr_dir
+
+# build the website
+bundle exec jekyll build --incremental
+
+sync with git
+git add *
+if [ $# -eq 0 ]; then
+    msg=auto_commit_msg_$RANDOM
+else
+    msg=$1
+fi
+
+git commit -m $msg
+git push origin main
